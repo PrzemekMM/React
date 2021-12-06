50f379abeaa45531ad7b98c8ea544be06efd3520
Przemek Mlodawski <przmlo09001@o2.pl>
2021-12-06 21:30:52 +0100

    react & webpack

---
+++ src/components/Icon/Icon.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {listData} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
+
+const Icon = props => (<i className={`fas fa-${props.name}`}></i>);
+
+export default Icon;
